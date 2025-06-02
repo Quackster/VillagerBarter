1d79ca6b39fbcab3c9b64fe829409a217f8bde27
Quackster <quacksterr.15@gmail.com>
2025-06-02 15:54:12 +1000

    Make work with Spigot

--- src/main/java/org/oldskooler/villagerbartering/listeners/VillagerTradeListener.java
+++ src/main/java/org/oldskooler/villagerbartering/listeners/VillagerTradeListener.java
@@ -1,30 +1,14 @@
 package org.oldskooler.villagerbartering.listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
 import org.bukkit.persistence.PersistentDataType;
 import org.oldskooler.villagerbartering.VillagerBarter;
-import org.bukkit.Sound;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.MerchantRecipe;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class VillagerTradeListener implements Listener {
     private final Random random;
@@ -72,16 +56,6 @@ public class VillagerTradeListener implements Listener {
     @EventHandler
     public void onVillagerPickupFood(EntityPickupItemEvent event) {
         if (isItemUnpickable(event.getItem())) {
-            Bukkit.getServer().getPlayer("Cleopatra_Vll").sendMessage(event.getEntity().getType().name() + " tried picking up " + event.getItem().getItemStack().getType().name());
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void onPlayerAttemptPickup(PlayerAttemptPickupItemEvent event) {
-        if (isItemUnpickable(event.getItem())) {
-            Bukkit.getServer().getPlayer("Cleopatra_Vll").sendMessage("Player tried picking up " + event.getItem().getItemStack().getType().name());
-
             event.setCancelled(true);
         }
     }
